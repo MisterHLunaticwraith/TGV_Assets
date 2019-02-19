a694da1214e7029002198ee3eaa99ae090d97e22
MisterHLunaticwraith <lunaticwraith@hotmail.com>
2019-02-19 12:41:10 +0100

    v1.0.2(nonrc up2)
    Ajout banque de données images tablette

--- TGV_TabPics.hpp
+++ TGV_TabPics.hpp
@@ -17,3 +17,468 @@ class TGV_V_Lemmymotorheadovitch
 captionText ="Lemmy Motorheadovitch";
 picture = "\TGV_Assets\paa\VillainsMugshots\lemmy.jpg";
 };
+
+class TGV_RadiosAddon //must be unique, can be anything,good practice is to tag with your handle.
+{
+captionText = "Répartition Radios"; //simple caption text, mandatory, leave "" if you do not want a caption
+picture = "\TGV_Assets\paa\TabPics\repradios.jpg"; //mandatory must be .jpg or .paa
+};
+class TGV_ComsigAddon //must be unique, can be anything,good practice is to tag with your handle.
+{
+captionText = "COMSIG"; //simple caption text, mandatory, leave "" if you do not want a caption
+picture = "\TGV_Assets\paa\TabPics\comsig.jpg"; //mandatory must be .jpg or .paa
+};
+
+class TGV_WeaponsPile
+{
+captionText = "Une cache d'armes"; 
+picture = "\TGV_Assets\paa\TabPics\weaponspile.jpg";
+};
+
+class TGV_MinimiShort
+{
+captionText = "Une minimi model Paras"; 
+picture = "\TGV_Assets\paa\TabPics\minimishort.jpg";
+};
+
+class TGV_Ak74Plum
+{
+captionText = "Une AK74 plum"; 
+picture = "\TGV_Assets\paa\TabPics\ak74plum.jpg";
+};
+
+class TGV_Mortar
+{
+captionText = "Un mortier"; 
+picture = "\TGV_Assets\paa\TabPics\mortar.jpg";
+};
+
+class TGV_Humvee
+{
+captionText = "Un Humvee"; 
+picture = "\TGV_Assets\paa\TabPics\humvee.jpg";
+};
+
+class TGV_558g33
+{
+captionText = "Une Eotech 558 avec un zoom G33"; 
+picture = "\TGV_Assets\paa\TabPics\558g33.jpg";
+};
+
+class TGV_Battlezone
+{
+captionText = "Un champ de bataille"; 
+picture = "\TGV_Assets\paa\TabPics\battlezone.jpg";
+};
+
+class TGV_lafarge
+{
+captionText = "Le cimentier Lafarge"; 
+picture = "\TGV_Assets\paa\TabPics\lafarge.jpg";
+};
+
+class TGV_RG33
+{
+captionText = "Un véhicule RG33"; 
+picture = "\TGV_Assets\paa\TabPics\RG33.jpg";
+};
+
+class TGV_SoldatsRusses
+{
+captionText = "Des soldats de l'armée russe."; 
+picture = "\TGV_Assets\paa\TabPics\soldatsrusses.jpg";
+};
+
+class TGV_Spetsnaz
+{
+captionText = "Insigne des Spetsnaz"; 
+picture = "\TGV_Assets\paa\TabPics\Spetsnaz.jpg";
+};
+
+class TGV_MarkV
+{
+captionText = "Un navire Mark V"; 
+picture = "\TGV_Assets\paa\TabPics\markV.jpg";
+};
+
+class TGV_DeltaIns
+{
+captionText = "L'insigne des Delta forces"; 
+picture = "\TGV_Assets\paa\TabPics\delta.jpg";
+};
+
+class TGV_eod1
+{
+captionText = "Un démineur"; 
+picture = "\TGV_Assets\paa\TabPics\eod1.jpg";
+};
+
+class TGV_Detonate
+{
+captionText = "Boom!"; 
+picture = "\TGV_Assets\paa\TabPics\detonate.jpg";
+};
+
+class TGV_Fob
+{
+captionText = "Une FOB (Forward Operating Base)"; 
+picture = "\TGV_Assets\paa\TabPics\fob.jpg";
+};
+
+class TGV_Academi
+{
+captionText = "Le logo de la C.M.P. Academi"; 
+picture = "\TGV_Assets\paa\TabPics\academi.jpg";
+};
+
+class TGV_DevgruIns
+{
+captionText = "Le logo du D.E.V.G.R.U."; 
+picture = "\TGV_Assets\paa\TabPics\devgruIns.jpg";
+};
+
+class TGV_IED
+{
+captionText = "Un I.E.D."; 
+picture = "\TGV_Assets\paa\TabPics\ied.jpg";
+};
+
+class TGV_natoUni
+{
+captionText = "Des uniformes de casques bleus"; 
+picture = "\TGV_Assets\paa\TabPics\natoU.jpg";
+};
+
+class TGV_c130
+{
+captionText = "Un avion C130"; 
+picture = "\TGV_Assets\paa\TabPics\c130.jpg";
+};
+
+class TGV_UnSoldiers
+{
+captionText = "Des soldats des nations unies"; 
+picture = "\TGV_Assets\paa\TabPics\unsoldiers.jpg";
+};
+
+class TGV_eodteam
+{
+captionText = "Une équipe E.O.D."; 
+picture = "\TGV_Assets\paa\TabPics\eodteam.jpg";
+};
+
+class TGV_MilMi8
+{
+captionText = "Un hélicoptère Mil Mi-8 des nations unies"; 
+picture = "\TGV_Assets\paa\TabPics\MilMi8.jpg";
+};
+
+class TGV_UH60BH
+{
+captionText = "Un UH-60 BlackHawk"; 
+picture = "\TGV_Assets\paa\TabPics\uh60bh.jpg";
+};
+
+class TGV_iedembush
+{
+captionText = "Une explosion d'IED sur un convoit américain"; 
+picture = "\TGV_Assets\paa\TabPics\iedembush.jpg";
+};
+
+class TGV_RussianForces
+{
+captionText = "Des forces de l'armée Russe"; 
+picture = "\TGV_Assets\paa\TabPics\russianforces.jpg";
+};
+
+class TGV_m320GL
+{
+captionText = "Un lance grenade M320"; 
+picture = "\TGV_Assets\paa\TabPics\m320gl.jpg";
+};
+
+class TGV_ACUD
+{
+captionText = "Un uniforme camouflage MARPAT"; 
+picture = "\TGV_Assets\paa\TabPics\marpat.jpg";
+};
+
+class TGV_MH6LB
+{
+captionText = "Un MH-6 Little Bird"; 
+picture = "\TGV_Assets\paa\TabPics\MH6LB.jpg";
+};
+
+class TGV_Gorka3
+{
+captionText = "Un uniforme Russe Gorka 3"; 
+picture = "\TGV_Assets\paa\TabPics\gorka3.jpg";
+};
+
+class TGV_mecity
+{
+captionText = "Une ville du Moyen Orient"; 
+picture = "\TGV_Assets\paa\TabPics\mecity.jpg";
+};
+
+class TGV_ak47
+{
+captionText = "Une AK-47"; 
+picture = "\TGV_Assets\paa\TabPics\ak47.jpg";
+};
+
+
+class TGV_MSF
+{
+captionText = "Un bénévole de médecins sans frontières"; 
+picture = "\TGV_Assets\paa\TabPics\msf.jpg";
+};
+
+class TGV_unHQ
+{
+captionText = "Le siège des Nations Unies"; 
+picture = "\TGV_Assets\paa\TabPics\unHQ.jpg";
+};
+
+class TGV_fastopscore
+{
+captionText = "Un casque F.A.S.T. Ballistic"; 
+picture = "\TGV_Assets\paa\TabPics\fastopscore.jpg";
+};
+
+class TGV_sem52sl
+{
+captionText = "Une SEM 52 SL"; 
+picture = "\TGV_Assets\paa\TabPics\sem52sl.jpg";
+};
+
+class TGV_Contractor
+{
+captionText = "Un contractor d'une C.M.P."; 
+picture = "\TGV_Assets\paa\TabPics\contractor.jpg";
+};
+
+class TGV_CiaLogo
+{
+captionText = "Le logo de la C.I.A."; 
+picture = "\TGV_Assets\paa\TabPics\cialogo.jpg";
+};
+
+class TGV_helmetcam
+{
+captionText = "Une caméra embarquée"; 
+picture = "\TGV_Assets\paa\TabPics\helmetcam.jpg";
+};
+
+class TGV_mp7sil
+{
+captionText = "Un MP-7 avec silencieux"; 
+picture = "\TGV_Assets\paa\TabPics\mp7sil.jpg";
+};
+
+class TGV_repfr
+{
+captionText = "Le logo de la république française."; 
+picture = "\TGV_Assets\paa\TabPics\repfr.jpg";
+};
+
+class TGV_talibans
+{
+captionText = "Des talibans"; 
+picture = "\TGV_Assets\paa\TabPics\talibans.jpg";
+};
+
+class TGV_milmi24
+{
+captionText = "Un Mil Mi-24"; 
+picture = "\TGV_Assets\paa\TabPics\milmi24.jpg";
+};
+
+class TGV_d30hw
+{
+captionText = "Une pièce d'artillerie D-30 Howitzer"; 
+picture = "\TGV_Assets\paa\TabPics\d30hw.jpg";
+};
+
+class TGV_files
+{
+captionText = "Des dossiers"; 
+picture = "\TGV_Assets\paa\TabPics\files.jpg";
+};
+
+class TGV_vladput
+{
+captionText = "Vladimir Poutine"; 
+picture = "\TGV_Assets\paa\TabPics\vladput.jpg";
+};
+
+class TGV_gpngv18
+{
+captionText = "Des JVN GP-NVG-18"; 
+picture = "\TGV_Assets\paa\TabPics\gpngv18.jpg";
+};
+
+class TGV_javelin
+{
+captionText = "Un Javelin"; 
+picture = "\TGV_Assets\paa\TabPics\javelin.jpg";
+};
+
+class TGV_seals
+{
+captionText = "Un groupe de SEALS"; 
+picture = "\TGV_Assets\paa\TabPics\seals.jpg";
+};
+
+class TGV_p35radar
+{
+captionText = "Un radar russe P35"; 
+picture = "\TGV_Assets\paa\TabPics\p35radar.jpg";
+};
+
+class TGV_m113
+{
+captionText = "Un véhicule de transport de troupes M113"; 
+picture = "\TGV_Assets\paa\TabPics\m113.jpg";
+};
+
+class TGV_mrzr4
+{
+captionText = "Un Polaris MRZR 4"; 
+picture = "\TGV_Assets\paa\TabPics\mrzr4.jpg";
+};
+
+class TGV_dgse
+{
+captionText = "Le logo de la DGSE"; 
+picture = "\TGV_Assets\paa\TabPics\dgse.jpg";
+};
+
+class TGV_randomtal
+{
+captionText = "Un PACS ennemi de garde"; 
+picture = "\TGV_Assets\paa\TabPics\randomtal.jpg";
+};
+
+class TGV_mn120
+{
+captionText = "Une JVN monoculaire russe VALDAY-MN120"; 
+picture = "\TGV_Assets\paa\TabPics\mn120.jpg";
+};
+
+class TGV_seal6
+{
+captionText = "Les FROGS de la SEAL team 6"; 
+picture = "\TGV_Assets\paa\TabPics\seal6.jpg";
+};
+
+class TGV_m1a2
+{
+captionText = "Un tank M1A2 Abrams"; 
+picture = "\TGV_Assets\paa\TabPics\m1a2.jpg";
+};
+
+class TGV_glock17
+{
+captionText = "Un Glock 17"; 
+picture = "\TGV_Assets\paa\TabPics\glock17.jpg";
+};
+
+class TGV_telescopicLadder
+{
+captionText = "Une échelle télescopique"; 
+picture = "\TGV_Assets\paa\TabPics\telescopic.jpg";
+};
+
+class TGV_anprc343
+{
+captionText = "Une radio AN/PRC 343"; 
+picture = "\TGV_Assets\paa\TabPics\anprc343.jpg";
+};
+
+class TGV_ravendrone
+{
+captionText = "Un drone Raven"; 
+picture = "\TGV_Assets\paa\TabPics\ravendrone.jpg";
+};
+
+class TGV_m4a1
+{
+captionText = "Un fusil d'assaut M4A1"; 
+picture = "\TGV_Assets\paa\TabPics\m4a1.jpg";
+};
+
+class TGV_zu232
+{
+captionText = "Une DCA ZU-23-2"; 
+picture = "\TGV_Assets\paa\TabPics\zu232.jpg";
+};
+
+class TGV_anpvs15
+{
+captionText = "Des JVN binoculaires AN/PVS-15"; 
+picture = "\TGV_Assets\paa\TabPics\anpvs15.jpg";
+};
+
+class TGV_pvs27
+{
+captionText = "Un fusil de précision équipé d'une optique AN/PVS 27 de vision nocturne"; 
+picture = "\TGV_Assets\paa\TabPics\pvs27.jpg";
+};
+
+class TGV_usmc
+{
+captionText = "Le logo de l'United States Marine Corps"; 
+picture = "\TGV_Assets\paa\TabPics\usmc.jpg";
+};
+
+class TGV_sniperm107
+{
+captionText = "Un  tireur d'élite avec fusil M107"; 
+picture = "\TGV_Assets\paa\TabPics\sniperm107.jpg";
+};
+
+class TGV_M14EBR
+{
+captionText = "Un fusil de précision M14EBR"; 
+picture = "\TGV_Assets\paa\TabPics\M14EBR.jpg";
+};
+
+class TGV_M24
+{
+captionText = "Un fusil de précision M24"; 
+picture = "\TGV_Assets\paa\TabPics\M24.jpg";
+};
+
+class TGV_M40A5 
+{
+captionText = "Un fusil de précision M40A5"; 
+picture = "\TGV_Assets\paa\TabPics\M40A5.jpg";
+};
+
+class TGV_M107
+{
+captionText = "Un fusil de précision antimatériel M107"; 
+picture = "\TGV_Assets\paa\TabPics\M107.jpg";
+};
+
+class TGV_M2010
+{
+captionText = "Un fusil de précision M2010"; 
+picture = "\TGV_Assets\paa\TabPics\M2010.jpg";
+};
+
+class TGV_fluVirus
+{
+captionText = "Un virus"; 
+picture = "\TGV_Assets\paa\TabPics\flu.jpg";
+};
+
+
+/*template
+class TGV_
+{
+captionText = ""; 
+picture = "\TGV_Assets\paa\TabPics\.jpg";
+};
+*/
